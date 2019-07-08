bb8b36d44b6b51eefae18a6eea053c68a2624b30
jshi <superbin2012@gmail.com>
2019-07-08 18:28:12 +0800

    Data binding to object and collection.

--- WpfDemos/DataBinding.xaml.cs
+++ WpfDemos/DataBinding.xaml.cs
@@ -12,19 +12,47 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace WpfDemos
 {
     /// <summary>
     /// Interaction logic for DataBinding.xaml
     /// </summary>
-    public partial class DataBinding : Window, INotifyPropertyChanged
+    public partial class DataBinding : Window//, INotifyPropertyChanged
     {
         public DataBinding()
         {
             InitializeComponent();
+            PersonDetails = new Person
+            {
+                Name = "Google",
+                Blog = "https://www.google.com",
+                Experience = 10
+            };
+            DataContext = PersonDetails;
+
+            Employees = new ObservableCollection<Employee>
+            {
+                new Employee
+                {
+                    FirstName = "Kunal", LastName = "Chowdhury",
+                    Department = "Software Division"
+                },
+                new Employee
+                {
+                    FirstName = "Michael", LastName = "Washington",
+                    Department = "Software Division"
+                },
+                new Employee
+                {
+                    FirstName = "John", LastName = "Strokes",
+                    Department = "Finance Department"
+                }
+            };
+            dataGrid.ItemsSource = Employees;
         }
-        private string _personName;
+        //private string _personName;
         public string Department { get => "Software Engineering"; }
         //public string PersonName {
         //    get => _personName;
@@ -35,7 +63,7 @@ namespace WpfDemos
         //    }
         //}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public event PropertyChangedEventHandler PropertyChanged;
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
@@ -52,6 +80,7 @@ namespace WpfDemos
         //}
 
 
+        //propdp snip
         public string PersonName
         {
             get { return (string)GetValue(PersonNameProperty); }
@@ -61,6 +90,30 @@ namespace WpfDemos
         // Using a DependencyProperty as the backing store for PersonName.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty PersonNameProperty =
             DependencyProperty.Register("PersonName", typeof(string), typeof(DataBinding), new PropertyMetadata(string.Empty));
+
+
+
+        public Person PersonDetails
+        {
+            get { return (Person)GetValue(PersonDetailsProperty); }
+            set { SetValue(PersonDetailsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PersonDetails.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PersonDetailsProperty =
+            DependencyProperty.Register("PersonDetails", typeof(Person), typeof(DataBinding), new PropertyMetadata(null));
+
+
+
+        public ObservableCollection<Employee> Employees
+        {
+            get { return (ObservableCollection<Employee>)GetValue(EmployeesProperty); }
+            set { SetValue(EmployeesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Employees.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EmployeesProperty =
+            DependencyProperty.Register("Employees", typeof(ObservableCollection<Employee>), typeof(DataBinding), new PropertyMetadata(null));
 
 
     }
---
+++ WpfDemos/TextBoxExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemos
+{
+    class TextBoxExtensions
+    {
+
+
+        public static bool GetSelectOnFocus(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SelectOnFocusProperty);
+        }
+
+        public static void SetSelectOnFocus(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SelectOnFocusProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SelectOnFocus.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectOnFocusProperty =
+            DependencyProperty.RegisterAttached("SelectOnFocus", typeof(bool), typeof(TextBoxExtensions), new PropertyMetadata(false, OnSelectOnFocusChanged));
+
+        private static void OnSelectOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox tb)
+            {
+                tb.GotFocus += (s, arg) =>
+                {
+                    tb.SelectAll();
+                };
+            }
+        }
+    }
+}
